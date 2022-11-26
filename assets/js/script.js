a5f7df32be534e08373f2ae8f93bf88c19ed9057
Elodie Meunier <elow.m@live.fr>
2022-11-26 16:48:16 +0100

    [IMP] - Finish static page sections for mobile, tablet and more

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,35 @@
 const toggleElement = function (elem) { elem.classList.toggle('active'); }
 
 const btnArrow = document.querySelector('[data-arrow-down]');
-const profile = document.querySelector('[data-profile]');
-console.log(btnArrow, profile)
+const profile = document.querySelector("[data-page='profile']");
 btnArrow.addEventListener('click', () => {toggleElement(profile);});
+
+// Event 
+// const pages = document.querySelectorAll('[data-page]');
+// for (let i = 0; i < pages.length; i++) {
+//     window.scrollTo(0, 0);
+// }
+
+
+// page navigation variables
+// const navigationLinks = document.querySelectorAll("[data-nav-link]");
+// const pages = document.querySelectorAll("[data-page]");
+
+
+// add event to all nav link
+// for (let i = 0; i < navigationLinks.length; i++) {
+//   navigationLinks[i].addEventListener("click", function () {
+
+//     for (let i = 0; i < pages.length; i++) {
+//       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+//         pages[i].classList.add("active");
+//         navigationLinks[i].classList.add("active");
+//         window.scrollTo(0, 0);
+//       } else {
+//         pages[i].classList.remove("active");
+//         navigationLinks[i].classList.remove("active");
+//       }
+//     }
+
+//   });
+// }
