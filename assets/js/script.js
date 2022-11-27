2eab2531abb0a49675fde6790ba7a8d87f7fa6f5
Elodie Meunier <elow.m@live.fr>
2022-11-27 18:39:41 +0100

    [IMP] - Manage the responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,35 +3,25 @@
 const toggleElement = function (elem) { elem.classList.toggle('active'); }
 
 const btnArrow = document.querySelector('[data-arrow-down]');
-const profile = document.querySelector("[data-page='profile']");
+const profile = document.querySelector("[data-aside='profile']");
 btnArrow.addEventListener('click', () => {toggleElement(profile);});
 
-// Event 
-// const pages = document.querySelectorAll('[data-page]');
-// for (let i = 0; i < pages.length; i++) {
-//     window.scrollTo(0, 0);
-// }
-
-
 // page navigation variables
-// const navigationLinks = document.querySelectorAll("[data-nav-link]");
-// const pages = document.querySelectorAll("[data-page]");
-
-
+const navLinks = document.querySelectorAll('[data-nav-link]');
+const pages = document.querySelectorAll('[data-page]');
+console.log('All elements: ', navLinks, pages)
 // add event to all nav link
-// for (let i = 0; i < navigationLinks.length; i++) {
-//   navigationLinks[i].addEventListener("click", function () {
-
-//     for (let i = 0; i < pages.length; i++) {
-//       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-//         pages[i].classList.add("active");
-//         navigationLinks[i].classList.add("active");
-//         window.scrollTo(0, 0);
-//       } else {
-//         pages[i].classList.remove("active");
-//         navigationLinks[i].classList.remove("active");
-//       }
-//     }
-
-//   });
-// }
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', function () {
+        for (let i = 0; i < pages.length; i++) {
+            if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+                pages[i].classList.add('active');
+                navLinks[i].classList.add('active');
+                window.scrollTo(0, 0);
+            } else {
+                pages[i].classList.remove('active');
+                navLinks[i].classList.remove('active');
+            }
+        }
+    });
+}
