e9a59f742b379c51f5ece35f6485198e7c808da8
Elodie Meunier <elow.m@live.fr>
2022-11-30 22:08:05 +0100

    [WIP] - Management project categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ btnArrow.addEventListener('click', () => {toggleElement(profile);});
 // page navigation variables
 const navLinks = document.querySelectorAll('[data-nav-link]');
 const pages = document.querySelectorAll('[data-page]');
-console.log('All elements: ', navLinks, pages)
+
 // add event to all nav link
 for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener('click', function () {
@@ -24,4 +24,22 @@ for (let i = 0; i < navLinks.length; i++) {
             }
         }
     });
+}
+
+// Manage projects
+const navProjects = document.querySelectorAll('[data-nav-project]')
+const projects = document.querySelectorAll('[data-project]')
+
+for (let i = 0; i < navProjects.length; i++) {
+    navProjects[i].addEventListener('click', function () {
+        for (let i = 0; i < projects.length; i++) {
+            if (this.dataset.navProject === projects[i].dataset.project || this.dataset.navProject === 'all') {
+                projects[i].classList.add('active');
+                navProjects[i].classList.add('active');
+            } else {
+                projects[i].classList.remove('active');
+                navProjects[i].classList.remove('active');
+            }
+        }
+    })
 }
